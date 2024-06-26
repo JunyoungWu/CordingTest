411437c97b887ad19cbd0f4dffaffae9470a18d2
DESKTOP-2NL88U7\ferdia <jyjyjy572@gmail.com>
2024-06-27 00:00:09 +0900

    [Upload] Programmers LV1 Password

---
+++ CordingTest/src/Programmers/Lv1/Password.java
@@ -0,0 +1,28 @@
+
+public class Password {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public String solution(String s, String skip, int index) {
+        String answer = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            for (int j = 0; j < index; j++) {
+                c += 1;
+                if (c > 'z') {
+                    c -= 26;
+                }
+                if (skip.contains(String.valueOf(c))) {
+                    j--;
+                }
+            }
+            answer += c;
+        }
+
+        return answer;
+    }
+
+}
